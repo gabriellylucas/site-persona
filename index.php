de07c889f0c810577d0d1faa73e74d8189fd61df
vhgbyyy <gaby.lucasdeoliveira@gmail.com>
2025-08-25 17:10:05 -0300

    index.php conexao do banco

--- index.php
+++ index.php
@@ -1,11 +1,52 @@
 <?php
-$page = $_GET['page'] ?? 'home';  
-$file = $page . '.php';
+require_once 'conexao.php'; 
 
-if (file_exists($file)) {
-    include $file; 
-} else {
-    header("HTTP/1.0 404 Not Found");
-    include '404.php'; 
+$page = $_GET['page'] ?? 'home';
+
+switch ($page) {
+
+    case 'clientes_listar':
+        require 'controllers/ClienteController.php';
+        $controller = new ClienteController($pdo);
+        $controller->listar();
+        break;
+
+    case 'clientes_cadastrar':
+        require 'controllers/ClienteController.php';
+        $controller = new ClienteController($pdo);
+        $controller->cadastrar();
+        break;
+
+    case 'produtos_listar':
+        require 'controllers/ProdutoController.php';
+        $controller = new ProdutoController($pdo);
+        $controller->listar();
+        break;
+
+    case 'produtos_cadastrar':
+        require 'controllers/ProdutoController.php';
+        $controller = new ProdutoController($pdo);
+        $controller->cadastrar();
+        break;
+
+    case 'pedidos_listar':
+        require 'controllers/PedidoController.php';
+        $controller = new PedidoController($pdo);
+        $controller->listar();
+        break;
+
+    case 'pedidos_cadastrar':
+        require 'controllers/PedidoController.php';
+        $controller = new PedidoController($pdo);
+        $controller->cadastrar();
+        break;
+
+    case 'home':
+        include 'home.php';
+        break;
+
+    default:
+        include '404.php';
 }
 ?>
+
